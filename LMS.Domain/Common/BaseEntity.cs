e893620a6ac8b78b8188963b554732236dbea479
Mahmoud921 <hoda1892003@gmail.com>
2026-04-05 06:19:25 +0200

    Initial commit: Add LMS Backend solution with .gitignore

---
+++ LMS.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Domain.Common
+{
+    public class BaseEntity
+    {
+    }
+}
---
+++ LMS.Infrastructure/Persistence/LMSDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Infrastructure.Persistence
+{
+    public class LMSDbContext:DbContext
+    {
+        public LMSDbContext(DbContextOptions<LMSDbContext> options) : base(options)
+        {
+        }
+    }
+}
