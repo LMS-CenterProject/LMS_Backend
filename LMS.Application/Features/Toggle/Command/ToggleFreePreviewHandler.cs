8466c0248711dc3207bb269b5f0629accd432a15
Mahmoud921 <hoda1892003@gmail.com>
2026-04-15 02:58:35 +0200

    LMS.Application(Features/Toggle) : Add Command For Toggle

---
+++ LMS.Application/Features/Toggle/Command/ToggleFreePreviewHandler.cs
@@ -0,0 +1,39 @@
+﻿
+using LMS.Domain.Interfaces;
+using LMS.Domain.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LMS.Application.Features.Lesson.Commands.ToggleFreePreview
+{
+    public sealed class ToggleFreePreviewHandler
+        : IRequestHandler<ToggleFreePreviewCommand, bool>
+    {
+        private readonly ILessonRepository _lessonRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ToggleFreePreviewHandler(
+            ILessonRepository lessonRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _lessonRepository = lessonRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(ToggleFreePreviewCommand request, CancellationToken cancellationToken)
+        {
+            var lesson = await _lessonRepository.GetByIdAsync(request.LessonId, cancellationToken);
+            if (lesson == null)
+                return false;
+
+            lesson.ToggleFreePreview();     // Business method من الـ Domain
+
+            _lessonRepository.Update(lesson);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
