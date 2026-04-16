9093c1001bb15230a1bfa60b49898a06b40240ef
Mahmoud921 <hoda1892003@gmail.com>
2026-04-16 03:30:08 +0200

    LMS.Application(Dtos) : Add Dtos (Quiz,Answer,Submit)

---
+++ LMS.Application/DTOs/Answers/AnswerDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Application.DTOs.Answers
+{
+    public class AnswerDto
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+    }
+}
---
+++ LMS.Application/DTOs/Submit/SubmitDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Application.DTOs.Submit
+{
+    public class SubmitDto
+    {
+        public Guid QuestionId { get; set; }
+        public List<Guid> SelectedAnswerIds { get; set; } = [];
+    }
+}
