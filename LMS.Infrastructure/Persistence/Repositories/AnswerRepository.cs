6242707c4a353cfee6393a0a6c7c47a0bfa08d75
Mahmoud921 <hoda1892003@gmail.com>
2026-04-16 03:32:22 +0200

    LMS.Infrastracture(Repository) : Add Repository (Quiz,Answer,Question)

---
+++ LMS.Infrastructure/Persistence/Repositories/AnswerRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LMS.Domain.Entities;
+using LMS.Domain.Interfaces.Repositories;
+
+namespace LMS.Infrastructure.Persistence.Repositories
+{
+    public class AnswerRepository
+    : Repository<Answer>, IAnswerRepository
+    {
+        private readonly LMSDbContext _context;
+
+        public AnswerRepository(LMSDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Answer>> GetByQuestionIdAsync(
+            Guid questionId,
+            CancellationToken ct = default)
+        {
+            return await _context.Answers
+                .Where(a => a.QuestionId == questionId)
+                .ToListAsync(ct);
+        }
+    }
+}
---
+++ LMS.Infrastructure/Persistence/Repositories/QuizRepository.cs
@@ -0,0 +1,36 @@
+﻿using LMS.Domain.Entities;
+using LMS.Domain.Interfaces.Repositories;
+using LMS.Infrastructure.Persistence;
+using LMS.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+public sealed class QuizRepository
+    : Repository<Quiz>, IQuizRepository
+{
+    private readonly LMSDbContext _context;
+
+    public QuizRepository(LMSDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<Quiz?> GetQuizWithQuestionsAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        return await _context.Quizzes
+            .Include(q => q.Questions)
+                .ThenInclude(q => q.Answers)
+            .FirstOrDefaultAsync(q => q.Id == id, cancellationToken);
+    }
+
+    
+    public async Task<IEnumerable<Quiz>> GetByCourseIdAsync(
+        Guid courseId,
+        CancellationToken ct = default)
+    {
+        return await _context.Quizzes
+            .Where(q => q.CourseId == courseId)
+            .ToListAsync(ct);
+    }
+}
