f0efa602ee09daf2588b703fad3048b37e097d6b
Mahmoud921 <hoda1892003@gmail.com>
2026-04-30 03:02:31 +0300

    LMS.API/Controllers : Add Quiz, Question, QuizAttemp and Answer Controllers.

---
+++ LMS.API/Controllers/QuestionController.cs
@@ -0,0 +1,82 @@
+﻿using LMS.Application.Features.Question.Command.CreateQuestion;
+using LMS.Application.Features.Question.Command.DeleteQuestion;
+using LMS.Application.Features.Question.Command.UpdateQuestion;
+using LMS.Application.Features.Question.Query.GetQuestionById;
+using LMS.Application.Features.Question.Query.GetQuestionByQuizId;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public QuestionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET api/questions/{id}
+        [HttpGet("{id}")]
+        [Authorize(Policy = "ReadQuiz")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _mediator.Send(
+                new GetQuestionByIdQuery(id));
+
+            return Ok(result);
+        }
+
+        // GET api/questions/quiz/{quizId}
+        [HttpGet("quiz/{quizId}")]
+        [Authorize(Policy = "ReadQuiz")]
+        public async Task<IActionResult> GetByQuiz(Guid quizId)
+        {
+            var result = await _mediator.Send(
+                new GetQuestionByQuizIdQuery(quizId));
+
+            return Ok(result);
+        }
+
+        // POST api/questions
+        [HttpPost]
+        [Authorize(Policy = "ManageQuestion")]
+        public async Task<IActionResult> Create(
+            CreateQuestionCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return Ok(id);
+        }
+
+        // PUT api/questions/{id}
+        [HttpPut("{id}")]
+        [Authorize(Policy = "ManageQuestion")]
+        public async Task<IActionResult> Update(
+            Guid id,
+            UpdateQuestionCommand command)
+        {
+            var updated = command with { QuestionId = id };
+
+            await _mediator.Send(updated);
+
+            return NoContent();
+        }
+
+        // DELETE api/questions/{id}
+        [HttpDelete("{id}")]
+        [Authorize(Policy = "ManageQuestion")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _mediator.Send(
+                new DeleteQuestionCommand(id));
+
+            return NoContent();
+        }
+    }
+}
---
+++ LMS.API/Controllers/QuizAttemptController.cs
@@ -0,0 +1,40 @@
+﻿using LMS.Application.Features.Quiz.Command.SubmitQuiz;
+using LMS.Application.Features.Quiz.Query.GetMyAttemp;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class QuizAttemptController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public QuizAttemptController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // 🔥 Submit Quiz
+        [HttpPost("submit")]
+        [Authorize(Policy = "ManageSubmit")]
+        public async Task<IActionResult> Submit(SubmitQuizCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        // 🔥 My Attempts
+        [HttpGet("my-attempts")]
+        [Authorize(Policy = "ManageSubmit")]
+        public async Task<IActionResult> GetMyAttempts()
+        {
+            var result = await _mediator.Send(new GetMyAttemptsQuery());
+            return Ok(result);
+        }
+    }
+}
