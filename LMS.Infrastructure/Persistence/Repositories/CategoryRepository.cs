abf38e2fa09d257dcd8db0afcf39009410879e8c
Mahmoud921 <hoda1892003@gmail.com>
2026-05-02 03:41:32 +0300

    Add ICategoryRepo and CategoryRepo

---
+++ LMS.Infrastructure/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using LMS.Domain.Entities;
+using LMS.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Infrastructure.Persistence.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(LMSDbContext context) : base(context)
+        {
+        }
+        public async Task<bool> ExistsByNameAsync(string name, CancellationToken ct)
+        {
+            return await DbSet.AnyAsync(c => c.Name == name, ct);
+        }
+    }
+}
