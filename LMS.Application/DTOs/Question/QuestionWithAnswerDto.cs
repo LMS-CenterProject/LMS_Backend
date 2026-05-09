648d23a1265c3cdd43646c68a1d9eda416bbd9a5
Mahmoud921 <hoda1892003@gmail.com>
2026-05-09 04:03:54 +0300

    LMS.Application/Dtos : Adding And Editing Dtos

--- LMS.Application/DTOs/Question/QuestionWithAnswerDto.cs
+++ LMS.Application/DTOs/Question/QuestionWithAnswerDto.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using LMS.Application.DTOs.Answers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LMS.Application.DTOs.Question
 {
-    public class QuestionDto
+    public class QuestionWithAnswersDto
     {
         public Guid Id { get; set; }
         public Guid QuizId { get; set; }
         public string Text { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
         public int Points { get; set; }
+
+        public List<AnswerDto> Answers { get; set; } = [];
     }
 }
