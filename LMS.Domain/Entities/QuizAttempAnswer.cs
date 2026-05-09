6b94032207cdec2a17c9f403c5df6525407722b9
Mahmoud921 <hoda1892003@gmail.com>
2026-05-09 04:01:14 +0300

    LMS.Domain/Entities : Add QuizAttempAnswer Model

---
+++ LMS.Domain/Entities/QuizAttempAnswer.cs
@@ -0,0 +1,26 @@
+﻿using LMS.Domain.Primitives;
+
+namespace LMS.Domain.Entities
+{
+    public sealed class QuizAttemptAnswer : Entity
+    {
+        private QuizAttemptAnswer() { }
+
+        public Guid QuizAttemptId { get; private set; }
+        public Guid QuestionId { get; private set; }
+        public Guid AnswerId { get; private set; }
+
+        // Navigation properties
+        public QuizAttempt Attempt { get; private set; } = null!;
+        public Question Question { get; private set; } = null!;
+        public Answer Answer { get; private set; } = null!;
+
+        public static QuizAttemptAnswer Create(Guid attemptId, Guid questionId, Guid answerId) =>
+            new()
+            {
+                QuizAttemptId = attemptId,
+                QuestionId = questionId,
+                AnswerId = answerId
+            };
+    }
+}
